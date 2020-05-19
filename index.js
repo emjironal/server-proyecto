9b69abc04bde132382a4a18cba0e952bb254c61c
Eduardo Jirón <emjironal@gmail.com>
2020-05-18 18:30:13 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const PORT = 8000
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'mysql',
+        database: 'menu'
+    }
+) //end createConnection
+
+db.connect((err) =>
+{
+    if (err)
+    {
+        throw err;
+    } //end if
+    console.log('Connected to database');
+}) //end connect
+
+global.db = db;
+
+const {getPlato, newPlato, deletePlato, updatePlato} = require('./routes/controller.js')
+
+app.get('/getPlato', getPlato)
+app.post('/newPlato', newPlato)
+app.post('/deletePlato', deletePlato)
+app.post('/updatePlato', updatePlato)
+
+app.listen(PORT, ()=>
+{
+    console.log('Server started at: http://localhost:8000')
+}) //end listen
---
+++ routes/controller.js
@@ -0,0 +1,120 @@
+module.exports = 
+{
+    getPlato: (req, res) =>
+    {
+        console.log("getPlato")
+        var selectPlato = "select id, nombre, precio from PLATO;"
+
+        db.query(selectPlato, function(err, rows)
+        {
+            if(err)
+            {
+                console.log("[error] getPlato. Query selectPlato " + err)
+                res.send({"error": true});
+            } //end if
+            else
+            {
+                res.send({"error": false, "platos": rows});
+            } //end else
+        }) //end selectPlato
+    }, //end getPlato
+
+    newPlato: (req, res) =>
+    {
+        db.beginTransaction(function(error)
+        {
+            if(error)
+            {
+                db.rollback()
+                console.log("[error] newPlato. beginTransaction " + error)
+                res.send({"error": true})
+            } //end if
+            else
+            {
+                var insertPlato = "insert into PLATO (NOMBRE, PRECIO) values (?, ?);"
+                var insertPlatoValues = [req.body.nombre, req.body.precio]
+
+                db.query(insertPlato, insertPlatoValues, function(err, rows)
+                {
+                    if(err)
+                    {
+                        db.rollback()
+                        console.log("[error] newPlato. Query insertPlato " + err)
+                        res.send({"error": true});
+                    } //end if
+                    else
+                    {
+                        db.commit()
+                        res.send({"error": false, "id": rows.insertId});
+                    } //end else
+                }) //end insertPlato
+            } //end else
+        }) //beginTransaction
+    }, //end newPlato
+
+    deletePlato: (req, res) =>
+    {
+        db.beginTransaction(function(error)
+        {
+            if(error)
+            {
+                db.rollback()
+                console.log("[error] deletePlato. beginTransaction " + error)
+                res.send({"error": true})
+            } //end if
+            else
+            {
+                var deletePlato = "delete from PLATO where id = ?;"
+                var deletePlatoValues = [req.body.id]
+
+                db.query(deletePlato, deletePlatoValues, function(err, rows)
+                {
+                    if(err)
+                    {
+                        db.rollback()
+                        console.log("[error] deletePlato. Query deletePlato " + err)
+                        res.send({"error": true});
+                    } //end if
+                    else
+                    {
+                        db.commit()
+                        res.send({"error": false});
+                    } //end else
+                }) //end deletePlato
+            } //end else
+        }) //end beginTransaction
+    }, //end deletePlato
+
+    updatePlato: (req, res) =>
+    {
+        db.beginTransaction(function(error)
+        {
+            if(error)
+            {
+                db.rollback()
+                console.log("[error] updatePlato. beginTransaction " + error)
+                res.send({"error": true})
+            } //end if
+            else
+            {
+                var updatePlato = "update PLATO set nombre = ?, precio = ? where id = ?;"
+                var updatePlatoValues = [req.body.nombre, req.body.precio, req.body.id]
+
+                db.query(updatePlato, updatePlatoValues, function(err, rows)
+                {
+                    if(err)
+                    {
+                        db.rollback()
+                        console.log("[error] updatePlato. Query updatePlato " + err)
+                        res.send({"error": true});
+                    } //end if
+                    else
+                    {
+                        db.commit()
+                        res.send({"error": false});
+                    } //end else
+                }) //end updatePlato
+            } //end else
+        }) //end beginTransaction
+    }, //end updatePlato
+} //end exports
